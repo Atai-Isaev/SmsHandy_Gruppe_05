913fcc30384045d955063e154fb101d3b69549cb
Bekzhan Satarov <satarovbekzhan@gmail.com>
2021-04-19 16:39:05 +0200

    about half of a PrepaidSmsHandy and TariffPlanSmsHandy methods implementation

--- src/smsHandy/model/PrepaidSmsHandy.java
+++ src/smsHandy/model/PrepaidSmsHandy.java
@@ -1,4 +1,47 @@
 package smsHandy.model;
 
-public class PrepaidSmsHandy {
+/**
+ * Klasse PrepaidSmsHandy. Ein Handy, das über ein beim
+ * Provider verwaltetes Guthaben verfügt und dessen
+ * SMS-Versand über dieses Guthaben abgerechnet wird.
+ *
+ * @version 1.0
+ * @author Bekzhan SATAROV
+ */
+public class PrepaidSmsHandy extends SmsHandy {
+
+    private static final int COST_PER_SMS = 10;
+
+    /**
+     * Konstruktor zum Erstellen eines neuen PrepaidHandy.
+     *
+     * @param number die Handynummer
+     * @param provider die Providerinstanz
+     */
+    public PrepaidSmsHandy(String number, Provider provider) {
+        super(number, provider);
+    }
+
+    /**
+     * Prüft, ob das Guthaben noch für das Versenden einer SMS reicht.
+     *
+     * @return ist das Guthaben noch ausreichend?
+     */
+    @Override
+    public boolean canSendSms() {
+        return false;
+    }
+
+    /**
+     * Zieht die Kosten für eine SMS vom Guhaben ab.
+     */
+    @Override
+    public void payForSms() {}
+
+    /**
+     * Lädt das Guthaben fuer das SmsHandy-Objekt auf.
+     *
+     * @param amount Menge, um die Aufgeladen werden soll
+     */
+    public void deposit(int amount) {}
 }
--- src/smsHandy/model/TariffPlanSmsHandy.java
+++ src/smsHandy/model/TariffPlanSmsHandy.java
@@ -1,4 +1,52 @@
 package smsHandy.model;
 
-public class TariffPlanSmsHandy {
+/**
+ * Klasse TariffPlanSmsHandy. Ein Vertragshandy, das über eine
+ * bestimmte Menge an Frei-SMS verfügt. In einer späteren Version
+ * könnten diese nach einer bestimmten Zeit wieder zurückgesetzt
+ * werden. Dies wird vorerst noch nicht berücksichtigt.
+ *
+ * @version 1.0
+ * @author Bekzhan SATAROV
+ */
+public class TariffPlanSmsHandy extends SmsHandy {
+
+    private int remainingFreeSms = 0;
+
+    /**
+     * Konstruktor zum Erstellen eines neuen TariffPlanHandy.
+     *
+     * @param number die Handynummer
+     * @param provider die Providerinstanz
+     */
+    public TariffPlanSmsHandy(String number, Provider provider) {
+        super(number, provider);
+    }
+
+    /**
+     * Prüft, ob Frei-SMS noch zum Senden ausreichen.
+     *
+     * @return noch Frei-SMS vorhanden?
+     */
+    @Override
+    public boolean canSendSms() {
+        return getRemainingFreeSms() > 0;
+    }
+
+    /**
+     * Reduziert die Frei-SMS.
+     */
+    @Override
+    public void payForSms() {
+        if (canSendSms()) this.remainingFreeSms--;
+    }
+
+    /**
+     * Liefert Anzahl der verbliebenen Frei-SMS.
+     *
+     * @return Anzahl der Frei-SMS
+     */
+    public int getRemainingFreeSms() {
+        return remainingFreeSms;
+    }
 }

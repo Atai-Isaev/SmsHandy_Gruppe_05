b086a4084271de1bc80c1c34d2ee19865019e58f
Atai Isaev <Atai.Isaev.kx7@fh-zwickau.de>
2021-04-19 21:38:36 +0200

    added method-implementations for PrepaidSmsHandy-class

--- src/smsHandy/model/PrepaidSmsHandy.java
+++ src/smsHandy/model/PrepaidSmsHandy.java
@@ -4,9 +4,6 @@ package smsHandy.model;
  * Klasse PrepaidSmsHandy. Ein Handy, das über ein beim
  * Provider verwaltetes Guthaben verfügt und dessen
  * SMS-Versand über dieses Guthaben abgerechnet wird.
- *
- * @version 1.0
- * @author Bekzhan SATAROV
  */
 public class PrepaidSmsHandy extends SmsHandy {
 
@@ -20,6 +17,9 @@ public class PrepaidSmsHandy extends SmsHandy {
      */
     public PrepaidSmsHandy(String number, Provider provider) {
         super(number, provider);
+        provider.register(this);
+        this.deposit(100);
+        System.out.println("Created new PrepaidSmsHandy: "+number);
     }
 
     /**
@@ -29,19 +29,29 @@ public class PrepaidSmsHandy extends SmsHandy {
      */
     @Override
     public boolean canSendSms() {
-        return false;
+        return this.getProvider().getCreditForSmsHandy(this.getNumber()) >= 10;
     }
 
     /**
      * Zieht die Kosten für eine SMS vom Guhaben ab.
      */
     @Override
-    public void payForSms() {}
+    public void payForSms() {
+        if (this.canSendSms()) this.deposit(-COST_PER_SMS);
+        else System.out.println("Method payForSms: your balance ="+this.getProvider().getCreditForSmsHandy(this.getNumber()));
+    }
 
     /**
      * Lädt das Guthaben fuer das SmsHandy-Objekt auf.
      *
      * @param amount Menge, um die Aufgeladen werden soll
      */
-    public void deposit(int amount) {}
+    public void deposit(int amount) {
+        if (amount!=0){
+            this.getProvider().deposit(this.getNumber(), amount);
+            System.out.println("Method deposit(PrepSmsHandy): +"+amount+". Now your credit is "+this.getProvider().getCreditForSmsHandy(this.getNumber()));
+        }
+        else System.out.println("Method deposit(PrepSmsHandy): amount is equals 0");
+
+    }
 }

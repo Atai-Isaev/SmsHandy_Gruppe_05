1fc3761f733878cf82b893d1177a59e7152ee970
Bekbolot <nurmanbetovbekbolot@gmail.com>
2021-05-09 01:20:44 +0200

    fix TODOs

--- src/test/SmsHandyTest.java
+++ src/test/SmsHandyTest.java
@@ -4,6 +4,7 @@ import main.smsHandy.exception.InvalidNumberException;
 import main.smsHandy.exception.ProviderNotFoundException;
 import main.smsHandy.exception.SmsHandyNotFoundException;
 import main.smsHandy.model.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,20 +13,33 @@ import java.util.Date;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmsHandyTest {
+    private Provider provider1;
+    private Provider provider2;
+    private PrepaidSmsHandy prepaidSmsHandyTelekom;
+    private PrepaidSmsHandy prepaidSmsHandyTelekom2;
+    private PrepaidSmsHandy prepaidSmsHandyBeeline;
+    TariffPlanSmsHandy tariffSmsHandyTelekom;
+    TariffPlanSmsHandy tariffSmsHandyTelekom2;
+    TariffPlanSmsHandy tariffSmsHandyBeeline;
 
+    @BeforeEach
+    public void init() throws ProviderNotFoundException {
+        provider1 = new Provider();
+        provider2 = new Provider();
+        provider1.setName("O!");
+        provider1.setName("Beeline");
+        prepaidSmsHandyTelekom = new PrepaidSmsHandy("123", provider1);
+        prepaidSmsHandyTelekom2 = new PrepaidSmsHandy("345", provider1);
+        prepaidSmsHandyBeeline = new PrepaidSmsHandy("222", provider2);
+        tariffSmsHandyTelekom = new TariffPlanSmsHandy("001", provider1);
+        tariffSmsHandyBeeline = new TariffPlanSmsHandy("002", provider2);
+
+    }
     // TODO: 07.05.2021 BeforeEach nutzen
 
     @Test
     @DisplayName("Test method-sendSms SmsHandy")
     public void testSendSmsSmsHandy() throws ProviderNotFoundException {
-        Provider provider = new Provider();
-        Provider provider1 = new Provider();
-        provider.setName("O!");
-        provider1.setName("Beeline");
-        PrepaidSmsHandy prepaidSmsHandyTelekom = new PrepaidSmsHandy("123", provider);
-        PrepaidSmsHandy prepaidSmsHandyTelekom2 = new PrepaidSmsHandy("345", provider);
-        PrepaidSmsHandy prepaidSmsHandyBeeline = new PrepaidSmsHandy("222", provider1);
-
         prepaidSmsHandyTelekom.sendSms("*101#", "");
         assertEquals(100, prepaidSmsHandyTelekom.getProvider().getCreditForSmsHandy(prepaidSmsHandyTelekom.getNumber()));
         prepaidSmsHandyTelekom.sendSms(prepaidSmsHandyTelekom2.getNumber(), "Hallo");
@@ -38,15 +52,6 @@ public class SmsHandyTest {
     @Test
     @DisplayName("Test method-sendSmsDirect SmsHandy")
     public void testSendSmsDirectSmsHandy() throws ProviderNotFoundException {
-
-        Provider provider = new Provider();
-        Provider provider1 = new Provider();
-        provider.setName("O!");
-        provider1.setName("Beeline");
-        PrepaidSmsHandy prepaidSmsHandyTelekom = new PrepaidSmsHandy("123", provider);
-        PrepaidSmsHandy prepaidSmsHandyTelekom2 = new PrepaidSmsHandy("345", provider);
-        TariffPlanSmsHandy tariffSmsHandyTelekom = new TariffPlanSmsHandy("001", provider);
-        TariffPlanSmsHandy tariffSmsHandyBeeline = new TariffPlanSmsHandy("002", provider1);
         prepaidSmsHandyTelekom.sendSmsDirect(prepaidSmsHandyTelekom2, "Direct message");
         assertEquals(100, prepaidSmsHandyTelekom.getProvider().getCreditForSmsHandy(prepaidSmsHandyTelekom.getNumber()));
         tariffSmsHandyTelekom.sendSmsDirect(tariffSmsHandyBeeline, "Some message");
@@ -57,10 +62,7 @@ public class SmsHandyTest {
 
     @Test
     @DisplayName("Test method-receiveSms SmsHandy")
-    public void testReceiveSmsSmsHandy() throws ProviderNotFoundException {
-        Provider provider = new Provider();
-        PrepaidSmsHandy prepaidSmsHandyTelekom = new PrepaidSmsHandy("123", provider);
-        PrepaidSmsHandy prepaidSmsHandyTelekom2 = new PrepaidSmsHandy("345", provider);
+    public void testReceiveSmsSmsHandy() {
         Message message = new Message();
         message.setFrom(null);
         message.setTo(prepaidSmsHandyTelekom2.getNumber());

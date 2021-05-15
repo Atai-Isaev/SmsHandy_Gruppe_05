36449f211ddb1138814b7dd23f105c7e971b38ae
Bekbolot <nurmanbetovbekbolot@gmail.com>
2021-05-15 12:51:10 +0200

    add functionality of adding new Provider to providersList

---
+++ src/main/smsHandy/view/ProviderEditDialogController.java
@@ -0,0 +1,106 @@
+package main.smsHandy.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import main.smsHandy.Main;
+import main.smsHandy.model.Provider;
+
+public class ProviderEditDialogController {
+    @FXML
+    private TextField providerNameField;
+
+
+    private Stage dialogStage;
+    private Provider provider;
+    private boolean okClicked = false;
+    private Main main;
+
+    /**
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+    @FXML
+    private void initialize() {
+    }
+
+    /**
+     * Sets the stage of this dialog.
+     *
+     * @param dialogStage
+     */
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+
+
+
+    /**
+     * Sets the provider to be edited in the dialog.
+     *
+     * @param provider
+     */
+    public void setProvider(Provider provider) {
+        this.provider = provider;
+        providerNameField.setText(provider.getName());
+    }
+
+    /**
+     * Returns true if the user clicked OK, false otherwise.
+     *
+     * @return
+     */
+    public boolean isOkClicked() {
+        return okClicked;
+    }
+
+    /**
+     * Called when the user clicks ok.
+     */
+    @FXML
+    private void handleOk() {
+        if (isInputValid()) {
+            provider.setName(providerNameField.getText());
+            okClicked = true;
+            dialogStage.close();
+        }
+    }
+
+    /**
+     * Called when the user clicks cancel.
+     */
+    @FXML
+    private void handleCancel() {
+        dialogStage.close();
+    }
+
+    /**
+     * Validates the user input in the text fields.
+     *
+     * @return true if the input is valid
+     */
+    private boolean isInputValid() {
+        String errorMessage = "";
+
+        if (providerNameField.getText() == null || providerNameField.getText().length() == 0) {
+            errorMessage += "No valid provider name!\n";
+        }
+
+
+        if (errorMessage.length() == 0) {
+            return true;
+        } else {
+            // Show the error message.
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.initOwner(dialogStage);
+            alert.setTitle("Invalid Fields");
+            alert.setHeaderText("Please correct invalid fields");
+            alert.setContentText(errorMessage);
+
+            alert.showAndWait();
+
+            return false;
+        }
+    }
+}
